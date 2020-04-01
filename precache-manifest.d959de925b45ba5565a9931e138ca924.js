11a87f72d5181418f1c6f3ce99be4f235f5eac89
AttilaPravda <attilabre@gmail.com>
2020-04-01 16:39:34 +0200

    Updates

---
+++ precache-manifest.d959de925b45ba5565a9931e138ca924.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "407906ea14ee163ad986b9a2a8c30ebc",
+    "url": "/OLJ/index.html"
+  },
+  {
+    "revision": "5bcd7ed9147e166ac19a",
+    "url": "/OLJ/static/css/2.1a6e4fba.chunk.css"
+  },
+  {
+    "revision": "54748885ceb0ae506904",
+    "url": "/OLJ/static/css/main.4102a2da.chunk.css"
+  },
+  {
+    "revision": "5bcd7ed9147e166ac19a",
+    "url": "/OLJ/static/js/2.a14015d1.chunk.js"
+  },
+  {
+    "revision": "c3a068adc9644cd928e6c03f2977358b",
+    "url": "/OLJ/static/js/2.a14015d1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "54748885ceb0ae506904",
+    "url": "/OLJ/static/js/main.6d290df8.chunk.js"
+  },
+  {
+    "revision": "182c2262572c426fbbc6",
+    "url": "/OLJ/static/js/runtime-main.e0cc90c2.js"
+  },
+  {
+    "revision": "34649f601ef69698c36331e4977f9c20",
+    "url": "/OLJ/static/media/Frame.34649f60.png"
+  },
+  {
+    "revision": "8755ed8af3e93dd61e116cb0b65c2766",
+    "url": "/OLJ/static/media/happy_couple.8755ed8a.jpg"
+  },
+  {
+    "revision": "fd050f8dc7d8de10ee71969010fb3c4d",
+    "url": "/OLJ/static/media/hero-area.fd050f8d.svg"
+  }
+]);
